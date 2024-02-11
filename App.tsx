bc1351f1c48e8cb5c93825410b5028a8947d63c3
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-02-11 12:22:21 -0300

    Create global theme style

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './src/styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ThemeProvider theme={theme}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  COLORS:{
+      black: '#000',
+      red: '#E25858'
+  },
+  FONT_FAMILY:{
+      REGULAR: 'Poppins_400Regular'
+  },
+  FONT_SIZE:{
+      sm: 12,
+      md: 14,
+      lg: 16
+  },
+  TEXT_DECORATION:{
+      td: 'line-through'
+  }
+}   
